659007e9569a8ab086df5b92321a50259492c11d
lorenz <lorenz.rosenthal@gmail.com>
2017-05-10 19:34:14 +0200

    MatrixType :: basic implementation done

--- utils.js
+++ utils.js
@@ -4,6 +4,33 @@ const utils = (function() {
 	 * GLOBAL TYPES
 	 */
 	
+	 /**
+	 * Matrix Datatype.
+	 * @param {Array} arr matrix rows
+	 */
+	window.Matrix = function(name, arr = [[0]]) {
+		let data = arr
+
+		this.name = name
+
+		this.rows = function(arg) {
+			if (!arg) {
+				// user wants all rows
+				return arg
+			}
+
+			if (!isNaN(arg)) {
+				// user wants certain row
+				return data[arg]
+			}
+			
+			if (isArr(arg) && isNaN(arg[0])) {
+				// user wants to get range of certain rows
+				return data.slice(arg[0], arg[1])
+			}
+		}
+
+	}
 
 	window.ℚ = function(a, b) {
 		this.a = a
